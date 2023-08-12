29b22ba367f93acbc67834958180c89347861c9e
Pawan123Pawan <105289150+Pawan123Pawan@users.noreply.github.com>
2023-08-13 00:21:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+const readBooks = library.filter(book => book.readingStatus === true);
+  
+  // Return the count of read books
+  return readBooks.length;
 };
 
 // Do not change the code below
